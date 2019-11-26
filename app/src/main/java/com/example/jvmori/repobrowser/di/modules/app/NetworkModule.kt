83f916bd2e90cab7cbf710a26b67fa25f587168f
jvMori <iwona.moryc92@gmail.com>
2019-11-26 19:08:51 +0100

    [di] created network module

---
+++ app/src/main/java/com/example/jvmori/repobrowser/di/modules/app/NetworkModule.kt
@@ -0,0 +1,49 @@
+package com.example.jvmori.repobrowser.di.modules.app
+
+import com.example.jvmori.repobrowser.data.GithubApi
+import com.example.jvmori.repobrowser.di.scopes.ApplicationScope
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+class NetworkModule {
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+    }
+
+    @Provides
+    @ApplicationScope
+    fun provideGithubApi(retrofit: Retrofit): GithubApi {
+        return retrofit.create<GithubApi>(GithubApi::class.java)
+    }
+
+    @Provides
+    @ApplicationScope
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @ApplicationScope
+    fun provideOkHttpClientBuilder(): OkHttpClient.Builder {
+        return OkHttpClient.Builder()
+    }
+
+    @Provides
+    @ApplicationScope
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .retryOnConnectionFailure(true)
+            .build()
+    }
+}
