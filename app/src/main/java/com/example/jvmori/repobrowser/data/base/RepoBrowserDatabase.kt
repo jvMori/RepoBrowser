e3dad26db243d80e5cb5781926b97ff3fdcb097a
Mori <iwona.moryc92@gmail.com>
2019-11-27 12:58:06 +0100

    [data] created type converter for saving list of repos in db and repo entity

--- app/src/main/java/com/example/jvmori/repobrowser/data/base/RepoBrowserDatabase.kt
+++ app/src/main/java/com/example/jvmori/repobrowser/data/base/RepoBrowserDatabase.kt
@@ -2,6 +2,10 @@ package com.example.jvmori.repobrowser.data.base
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.jvmori.repobrowser.data.repos.response.ReposResponse
+import com.example.jvmori.repobrowser.utils.Converters
 
-@Database(entities = [], version = 1, exportSchema = false)
+@Database(entities = [ReposResponse::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class RepoBrowserDatabase : RoomDatabase(){}
--- app/src/main/java/com/example/jvmori/repobrowser/data/repos/response/ReposResponse.kt
+++ app/src/main/java/com/example/jvmori/repobrowser/data/repos/response/ReposResponse.kt
@@ -1,13 +1,21 @@
 package com.example.jvmori.repobrowser.data.repos.response
 
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "repos_table", primaryKeys = ["repo_query", "repo_page"])
 data class ReposResponse(
+    @ColumnInfo(name = "repo_query")
+    var query : String,
+    @ColumnInfo(name = "repo_page")
+    var page : Int,
     @SerializedName("incomplete_results")
     var incompleteResults: Boolean,
     @SerializedName("items")
     var repositories: List<Repo>,
     @SerializedName("total_count")
-    var totalCount: Int
+    var totalCount: Int,
+    var timestamp : Long
 )
